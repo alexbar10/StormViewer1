ebe636e72ad46eadcbb565b813a9a54a4b0ccd6c
alejandrobarranco <alejandro.barranco@geoenlace.com>
2020-04-28 18:25:15 -0500

    feat(): Setting up, Listing images with FileManager, Designing our interface

--- StormViejer/ViewController.swift
+++ StormViejer/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.delegate = self
+        }
+    }
+    
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nss") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+        tableView.reloadData()
     }
-
-
 }
 
+
+/**
+ Extension for tableview methods
+ */
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pictureCellIdentifier", for: indexPath) as! CustomTableViewCell
+        
+        cell.pictureTitleLabel.text = pictures[indexPath.row]
+        
+        return cell
+    }
+    
+}
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var pictureTitleLabel: UILabel!
+}
